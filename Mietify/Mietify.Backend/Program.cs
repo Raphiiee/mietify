75ae2ac3fc8ec73080aaadb461563b111d559305
Maximiliano Zulli <if19b183@dev.azure.com>
2023-03-25 16:05:38 +0100

    docker compose - Mietify is http now

--- Mietify/Mietify.Backend/Program.cs
+++ Mietify/Mietify.Backend/Program.cs
@@ -22,7 +22,7 @@ namespace Mietify.Backend
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
